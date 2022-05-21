2b86673850e0f7858004ad756149c4c08e9c18fe
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-05-21 14:28:45 -0300

    Controllers

--- src/controllers/ApiController.js
+++ src/controllers/ApiController.js
@@ -1,59 +1,8 @@
 const fetch = require('node-fetch');
 
 module.exports = class ApiController {
-  static Favorites = async (req, res) => {};
+  static GetFavorites = async (req, res) => {};
 
-  static Search = async (req, res) => {
-    const letters = req.params.letters;
-    try {
-      const response = await fetch(
-        `www.thecocktaildb.com/api/json/v1/1/search.php?f=${letters}`,
-      );
-      const drink = await response.json();
-
-      res.status(200).json(drink);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-  };
-
-  static GetById = async (req, res) => {
-    const id = req.params.id;
-    try {
-      const response = await fetch(
-        `www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`,
-      );
-      const drink = await response.json();
-
-      res.status(200).json(drink);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-  };
-
-  static GetRandom = async (req, res) => {
-    try {
-      const response = await fetch(
-        'https://www.thecocktaildb.com/api/json/v1/1/random.php',
-      );
-      const drink = await response.json();
-      res.status(200).json(drink);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-  };
-
-  static GetList = async (req, res) => {
-    const type = req.params.type;
-    try {
-      const response = await fetch(
-        `www.thecocktaildb.com/api/json/v1/1/search.php?i=${id}`,
-      );
-      const drink = await response.json();
-
-      res.status(200).json(drink);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
+  static PushFavorite = async (req, res) => {};
   };
 };
--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -1,5 +1,71 @@
-module.exports = class ApiController {
+const mongoose = require('mongoose');
+const User = require('../models/user');
 
-  static SignIn = (req, res) => {}
-  static SignUp = (req, res) => {}
+module.exports = class LoginApiController {
+  static Login(req, res) {
+    if (!req.body.user || !req.body.password) {
+      res.status(406).json({
+        message: 'Without User or Password',
+        error: true,
+      });
+    } else {
+      User.findOne({ user: req.body.user }, (error, user) => {
+        if (error) {
+          res.status(500).json({
+            message: 'Internal Server Error!',
+            error: true,
+          });
+        } else if (user) {
+          if (req.body.password === user.password) {
+            res.status(200).json({ message: 'Success', error: false });
+          } else {
+            res.status(401).json({ message: 'Wrong Password', error: true });
+          }
+        } else {
+          res.status(406).json({ message: 'User not Found', error: true });
+        }
+      });
+    }
+  }
+
+  static Register(req, res) {
+    if (!req.body.user || !req.body.password) {
+      res.status(406).json({
+        message: 'Without User or Password',
+        error: true,
+      });
+    } else {
+      User.findOne({ user: req.body.user }, (error, user) => {
+        if (error) {
+          res.status(500).json({
+            message: 'Internal Server Error!',
+            error: true,
+          });
+        } else if (user) {
+          res.status(406).json({
+            message: 'This User already Exists',
+            error: true,
+          });
+        } else {
+          let NewUser = {
+            email: req.body.user,
+            password: req.body.password,
+            favorites: [],
+          };
+          try {
+            User.crate(NewUser);
+            res.status(201).json({
+              message: 'User Created with Success!',
+              error: false,
+            });
+          } catch (error) {
+            res.status(500).json({
+              message: 'Internal Server Error while Creating User!',
+              error: true,
+            });
+          }
+        }
+      });
+    }
+  }
 };
