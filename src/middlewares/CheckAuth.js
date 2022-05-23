0f12946a216cf9104a03991051be4a7280337167
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-05-22 21:28:28 -0300

    Create CheckAuth.js

---
+++ src/middlewares/CheckAuth.js
@@ -0,0 +1,25 @@
+module.exports.checkAuth = (req, res, next) => {
+  if (!req.body.email || !req.body.password) {
+    res.status(401).json({ message: 'Please log in before!', error: true });
+  } else {
+    User.findOne({ email: req.body.email }, (error, user) => {
+      if (error) {
+        res.status(500).json({
+          message: 'Internal Server Error!',
+          error: true,
+        });
+      } else if (user) {
+        if (req.body.password === user.password) {
+          next();
+        } else {
+          res.status(401).json({ message: 'Wrong Password!', error: true });
+        }
+      } else {
+        res.status(406).json({
+          message: 'User not Found! Please create an account before!',
+          error: true,
+        });
+      }
+    });
+  }
+};
