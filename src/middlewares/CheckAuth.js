e64c6dce711b720504e774fac2a2158ba962e76f
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-06-01 20:19:01 -0300

    Update CheckAuth.js

--- src/middlewares/CheckAuth.js
+++ src/middlewares/CheckAuth.js
@@ -1,28 +1,18 @@
-const mongoose = require('mongoose');
-const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 module.exports.checkAuth = (req, res, next) => {
-  if (!req.body.email || !req.body.password) {
-    res.status(401).json({ message: 'Please log in before!', error: true });
-  } else {
-    User.findOne({ email: req.body.email }, (error, user) => {
-      if (error) {
-        res.status(500).json({
-          message: 'Internal Server Error!',
-          error: true,
-        });
-      } else if (user) {
-        if (req.body.password === user.password) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Wrong Password!', error: true });
-        }
-      } else {
-        res.status(406).json({
-          message: 'User not Found! Please create an account before!',
-          error: true,
-        });
-      }
-    });
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Access Denied', error: true });
+  }
+
+  try {
+    const secret = process.env.SECRET;
+    jwt.verify(token, secret);
+    next();
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid Token', error: true });
   }
 };
