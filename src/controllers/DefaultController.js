8e3f339d4591523485a772ac6c303daa0cde6ec8
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-06-03 17:46:36 -0300

    Update DefaultController.js

--- src/controllers/DefaultController.js
+++ src/controllers/DefaultController.js
@@ -1,5 +1,13 @@
+const mongoose = require('mongoose');
+const Requests = require('../models/requests');
+
 module.exports = class ApiController {
   static ShowHome = async (req, res) => {
-    res.render('pages/defaultpage', { number: 10 });
+    try {
+      const requests = await Requests.findOne({ index: 1 });
+      res.render('pages/defaultpage', { number: requests.requests });
+    } catch (error) {
+      res.render('pages/defaultpage', { number: 'Error' });
+    }
   };
 };
