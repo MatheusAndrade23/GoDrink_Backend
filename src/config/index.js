7a19289f9825338e2630e2e306e2f18abae07e86
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2022-07-23 21:02:10 -0300

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,9 @@
 const config = {
   options: {
     origin: [
+      'https://go-drink-next-matheusandrade23.vercel.app',
+      'https://go-drink-next.vercel.app',
+      'https://go-drink-next-git-main-matheusandrade23.vercel.app',
       'https://go-drink-matheusandrade23.vercel.app',
       'https://go-drink-git-master-matheusandrade23.vercel.app',
       'https://go-drink.vercel.app',
