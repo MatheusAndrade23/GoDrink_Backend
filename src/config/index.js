e21f8be8ef238ed0e3519be2f4f7494c345eeb54
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2022-08-10 18:55:57 -0300

    New URLs

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,13 @@
 const config = {
   options: {
     origin: [
-      'https://go-drink-next-matheusandrade23.vercel.app',
-      'https://go-drink-next.vercel.app',
-      'https://go-drink-next-git-main-matheusandrade23.vercel.app',
+      'https://go-drink-react-matheusandrade23.vercel.app',
+      'https://go-drink-react.vercel.app',
+      'https://go-drink-react-git-master-matheusandrade23.vercel.app',
       'https://go-drink-matheusandrade23.vercel.app',
-      'https://go-drink-git-master-matheusandrade23.vercel.app',
+      'https://go-drink-git-main-matheusandrade23.vercel.app',
       'https://go-drink.vercel.app',
+      // 'http://localhost:3000',
     ],
   },
 };
